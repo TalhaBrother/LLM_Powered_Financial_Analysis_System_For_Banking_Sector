78cf609a7078c628978360659ae6ee7b682c37e3
Talha Brother <talhaikram262005@gmail.com>
2026-05-11 19:09:53 +0500

    PostgreSQL Connection with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-from app.routes.loan_route import router as loan_router
+from .routes.loan_route import router as loan_router
+from .database import engine,Base
+
 
 app = FastAPI()
 app.include_router(loan_router)
 
 
-
+Base.metadata.create_all(bind=engine)
+@app.get("/")
+def connect():
+    return {"message": "PostgreSQL is connected!"}
